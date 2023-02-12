97f08717c8acc58d94eb71e3d865eed7e6744963
Alan Paiva <allanpaiva21@outlook.com>
2023-02-12 20:20:05 -0300

    fix: tests adjested

--- internal/entity/product_test.go
+++ internal/entity/product_test.go
@@ -7,28 +7,35 @@ import (
 )
 
 func TestNewProduct(t *testing.T) {
-	product, err := NewProduct("product 1", 5000)
+	p, err := NewProduct("Product 1", 10)
 	assert.Nil(t, err)
-	assert.NotNil(t, product)
-	assert.NotEmpty(t, product.ID)
-	assert.Equal(t, "product 1", product.Name)
-	assert.Equal(t, 5000, product.Price)
-
+	assert.NotNil(t, p)
+	assert.NotEmpty(t, p.ID)
+	assert.Equal(t, "Product 1", p.Name)
+	assert.Equal(t, 10.0, p.Price)
 }
 
 func TestProductWhenNameIsRequired(t *testing.T) {
-	product, err := NewProduct("", 5000)
-	assert.Nil(t, product)
+	p, err := NewProduct("", 10)
+	assert.Nil(t, p)
 	assert.Equal(t, ErrNameIsRequired, err)
 }
 
 func TestProductWhenPriceIsRequired(t *testing.T) {
-	product, err := NewProduct("product 1", 0)
-	assert.Nil(t, product)
+	p, err := NewProduct("Product 1", 0)
+	assert.Nil(t, p)
 	assert.Equal(t, ErrPriceIsRequired, err)
 }
+
 func TestProductWhenPriceIsInvalid(t *testing.T) {
-	product, err := NewProduct("product 1", -1)
-	assert.Nil(t, product)
+	p, err := NewProduct("Product 1", -10)
+	assert.Nil(t, p)
 	assert.Equal(t, ErrInvalidPrice, err)
+}
+
+func TestProductValidate(t *testing.T) {
+	p, err := NewProduct("Product 1", 10)
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.Nil(t, p.Validate())
 }
